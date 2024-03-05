af63fd8f51d325edf0bc975d54ab4bf82b1b94d6
aryanipuspitasari <aryanipuspitasari92@gmail.com>
2024-03-05 19:01:37 +0100

    chatComponent.jsx is done, need to render the backend to frontend

--- frontend/src/component/05-main.jsx
+++ frontend/src/component/05-main.jsx
@@ -1,21 +1,31 @@
 import { useContext } from "react";
 import { LogInContext } from "../context/LogInContext.jsx";
 import { UsernameContext } from "../context/UsernameContext.jsx";
+import Chat from "./06-chat.jsx";
 
-function Main(){
-    const { setIsLoggedIn } = useContext(LogInContext);
-    const { username} = useContext(UsernameContext)
+function Main() {
+  const { setIsLoggedIn } = useContext(LogInContext);
+  const { username } = useContext(UsernameContext);
 
-    const handleLogout = () => {
-      setIsLoggedIn(false);
-    };
-  
-    return (
-      <div className="mainContainer">
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className="mainContainer">
+
+      <header className="mainHeader">
         <h1>Hai {username}</h1>
-        <button onClick={handleLogout}>Logout</button>
+        <button onClick={handleLogout} className="logoutButton">
+          X
+        </button>
+      </header>
+      
+      <div className="chatContainer">
+        <Chat />
       </div>
-    );
+    </div>
+  );
 }
 
-export default Main
+export default Main;
---
+++ frontend/src/component/06-chat.jsx
@@ -0,0 +1,13 @@
+function Chat() {
+  return (
+    
+
+    <div className="chatform">
+      <input type="text" placeholder="Type your message..." />
+      <button className="send-button">Send</button>
+    </div>
+    
+  );
+}
+
+export default Chat;
