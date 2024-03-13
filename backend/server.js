4eeb644f846736acc2e9eff62c217326ea03ede0
aryanipuspitasari <aryanipuspitasari92@gmail.com>
2024-03-13 15:32:52 +0100

    edit the middleware app.use cors

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded( { extended : true } ) );
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: [ "http://localhost:5173"] }));
+app.use(cors({ credentials: true }));
 connect();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
