fd6698b596298c2e890a3cb34780f7929f00151d
aryanipuspitasari <aryanipuspitasari92@gmail.com>
2024-03-13 15:18:51 +0100

    change the path import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { dirname, path } from "path";
+import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 // IMPORT MIDDLEWARE
